53010ad388ca36dbaaf78a94b44331cf6de1f947
Alan Inglis <alan.n.inglis@gmail.com>
2022-07-12 15:27:14 +0100

    added new plot type

--- R/splitDensity.R
+++ R/splitDensity.R
@@ -3,6 +3,7 @@
 #' @description Density plots of the split value for each variable.
 #'
 #' @param treeData A list of trees created using the treeData function.
+#' @param data Data frame containing variables from the model.
 #' @param colBy A parameter used to control the colour of the density plots.
 #' @param display Choose how to display the plot. Either histogram, facet wrap or ridges.
 #'
@@ -18,10 +19,10 @@
 #' @export
 
 
-splitDensity <- function(treeData, colBy = NULL, display = "histogram") {
+splitDensity <- function(treeData, data, colBy = NULL, display = "histogram") {
 
-  if (!(display %in% c("histogram", "ridge", "density"))) {
-    stop("display must be \"histogram\", \"ridge\", or \"density\"")
+  if (!(display %in% c("histogram", "ridge", "density", 'both'))) {
+    stop("display must be \"histogram\", \"ridge\", \"density\", or \"both\"")
   }
 
   # get just the variable and split value
@@ -33,6 +34,8 @@ splitDensity <- function(treeData, colBy = NULL, display = "histogram") {
   # create plotting order to match order of data
   nam <- treeData$varName
   tt$var <- factor(tt$var, levels = nam)
+
+  varNames <- unique(tt$var)
 
   # create plot
 
@@ -62,6 +65,25 @@ splitDensity <- function(treeData, colBy = NULL, display = "histogram") {
       ylab("Density") +
       xlab("Split value") +
       theme(legend.position = "none")
+  }else if(display == 'both'){
+
+    dataIdx <- which((names(data) %in% varNames))
+    dat <- data[, dataIdx]
+
+    meltDat <- melt(dat)
+    names(tt) <- c('variable', 'value')
+
+    dataList <- list(meltDat, tt)
+    names(dataList)  <- c('dat', 'sv')
+    dfList <- plyr::ldply(dataList)
+
+   dPlot <- ggplot(dfList) +
+      geom_density(aes(x = value, fill = .id), alpha = 0.5) +
+      facet_wrap(~variable) +
+      scale_fill_discrete(name = "", labels = c("Data", "Split Value")) +
+      ylab('Density') +
+      xlab("Split value") +
+      theme_bw()
   }
 
   suppressMessages(print(dPlot))
