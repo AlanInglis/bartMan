a4a3527a71be3e1d3c9a86f14d5db05f0f6d160f
Alan Inglis <alaninglis@Alans-Air-2.station>
2024-04-09 12:27:29 +0100

    Added very small dataset examples

---
+++ R/input_data.R
@@ -0,0 +1,16 @@
+#' input_data
+#'
+#' Small example of Friedman data following the formula:
+#' \deqn{y = 10 \sin(\pi x_1 x_2) + 20 (x_3 - 0.5)^2 + 10 x_4 + 5 x_5 + e}
+#'
+#' @format A data frame with 10 rows and 6 columns:
+#' \describe{
+#'   \item{x1}{Covariate}
+#'   \item{x2}{Covariate}
+#'   \item{x3}{Covariate}
+#'   \item{x4}{Covariate}
+#'   \item{x5}{Covariate}
+#'   \item{y}{Response}
+#'   ...
+#' }
+"input_data"
---
+++ R/tree_data_example.R
@@ -0,0 +1,14 @@
+#' tree_data_example
+#'
+#' Small example of tree data, like that obtained when using `extractTreeData()` function.
+#'
+#' @format A data frame with 14 rows and 4 columns representing the structure of trees:
+#' \describe{
+#'   \item{var}{Variable name used for splitting.}
+#'   \item{value}{The value in a node (i.e., either the split value or leaf value).}
+#'   \item{iteration}{Iteration Number.}
+#'   \item{treeNum}{Tree Number in the iteration.}
+#'   ...
+#' }
+"tree_data_example"
+
